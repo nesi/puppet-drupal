9714793a8add741baf2cb8df3abb1adab40f807c
Aaron Hicks <Aethylred@gmail.com>
2012-08-28 16:54:18 +1200

    First go with a cleanup script, be careful with it, git reset --hard HEAD should save you

---
+++ cleanup.ps1
@@ -0,0 +1,15 @@
+echo "Cleaning up template..."
+if ((Test-Path 'Modulefile') -and (Test-Path 'manifests/init.pp') -and (Test-Path 'test/init.pp.orig')){
+	echo "Deleting .orig templates..."
+	objFSO.DeleteFile('Modulefile.orig')
+	objFSO.DeleteFile('manifests/init.pp.orig')
+	objFSO.DeleteFile('tests/init.pp.orig')
+	objFSO.DeleteFile('README1st.markdown')
+	objFSO.DeleteFile('unblank.ps1')
+	objFSO.DeleteFile('cleanup.ps1')
+} else {
+	throw "ERROR: Cannot find required files, have you run unblank.ps1?"
+}
+if (Test-Path manifests\init.pp){
+	del manifests\init.pp.orig
+}
