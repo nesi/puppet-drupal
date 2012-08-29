8524cc97aaf9cf8dfc574bb7e07e5526e1a3eb6c
Aaron Hicks <Aethylred@gmail.com>
2012-08-29 15:42:03 +1200

    using the incorrect cmdlet

--- cleanup.ps1
+++ cleanup.ps1
@@ -1,15 +1,12 @@
 echo "Cleaning up template..."
 if ((Test-Path 'Modulefile') -and (Test-Path 'manifests/init.pp') -and (Test-Path 'tests/init.pp')){
 	echo "Deleting .orig templates..."
-	objFSO.DeleteFile('Modulefile.orig')
-	objFSO.DeleteFile('manifests/init.pp.orig')
-	objFSO.DeleteFile('tests/init.pp.orig')
-	objFSO.DeleteFile('README1st.markdown')
-	objFSO.DeleteFile('unblank.ps1')
-	objFSO.DeleteFile('cleanup.ps1')
+	Remove-Item 'Modulefile.orig'
+	Remove-Item 'manifests/init.pp.orig'
+	Remove-Item 'tests/init.pp.orig'
+	Remove-Item 'README1st.markdown'
+	Remove-Item 'unblank.ps1'
+	Remove-Item 'cleanup.ps1'
 } else {
 	throw "ERROR: Cannot find required files, have you run unblank.ps1?"
-}
-if (Test-Path manifests\init.pp){
-	del manifests\init.pp.orig
 }
